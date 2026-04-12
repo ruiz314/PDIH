908512d6e330a68e33d2f58011fe7fceacd66091
ruiz314 <64708177+ruiz314@users.noreply.github.com>
2026-04-12 13:09:48 +0200

    detector distancia
    Removed commented delay and noTone calls for buzzer control.

---
+++ P3/ficheros/ejer4.ino
@@ -0,0 +1,43 @@
+const int trigPin = 12;
+const int echoPin = 11;
+const int buzzPin = 10;
+
+void setup()
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzPin, OUTPUT);
+  Serial.begin(9600); // Para monitor serie
+}
+
+void loop()
+{
+  long duracion;
+  int distancia;
+  
+  // Disparo ultrasónico
+  digitalWrite(trigPin, LOW);
+  delay(2); // Wait for 2 milliseconds
+  digitalWrite(trigPin, HIGH);
+  delay(2); // Wait for 2 milliseconds
+  digitalWrite(trigPin, LOW);
+  
+  // Leer duración
+  duracion = pulseIn(echoPin, HIGH);
+  
+  //Calcular distancia
+  distancia = duracion * 0.034 / 2;
+  
+  Serial.print("Distancia: ");
+  Serial.println(distancia);
+  
+  //Control de buzzer según la distancia
+  if (distancia <= 60){
+  	tone(buzzPin, 1400, 600); 
+  }else if(distancia >60 && distancia <= 150){
+  	tone(buzzPin, 900, 400);
+  }else{ // Silencio si no hay objeto cerca
+  	noTone(buzzPin);
+  }
+  delay(100);
+}

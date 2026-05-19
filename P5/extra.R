fdbc37f1ea3696d2059aea78423f188027946cdd
ruiz314 <64708177+ruiz314@users.noreply.github.com>
2026-05-19 22:19:31 +0200

    Add echo effect and reverse sound functionalities
    Added functionality to apply echo effect to a sound file and save it as 'eco.wav'. Also implemented reversing the sound and saving it as 'alreves.wav'.

--- P5/extra.R
+++ P5/extra.R
@@ -45,3 +45,35 @@ plot(extractWave(filtro, from = 1, to = 81886))
 # Ejercicio 8. Tomar el sonido que se creó antes (lo tendremos en el archivo llamado “basico.wav”) 
 # para aplicarle el efecto de eco. Guardar ese sonido en un archivo nuevo llamado “eco.wav”. 
 # A continuación, se le debe dar la vuelta al sonido y almacenarlo como un fichero llamado “alreves.wav”
+
+
+# Generación de eco
+filtroECO <- echo(filtro, f=basico@samp.rate, amp=c(0.8,0.4,0.2), delay=c(1,2,3), output="Wave")
+
+# Escuchar audio
+listen(filtroECO) 
+
+# Mostrar información del sonido
+str(filtroECO) 
+filtroECO
+
+plot( extractWave(filtroECO, from = 1, to = 225886) ) 
+
+# Guardar en "eco.wav"
+writeWave(filtroECO, file.path("eco.wav") )
+
+# Dar la vuelta
+alreves <- revw(filtroECO, output="Wave") 
+
+# Escuchar sonido
+listen(alreves) 
+
+# Guardar en "alreves.wav"
+writeWave(alreves, file.path("alreves.wav") )
+
+# Mostrar información
+str(alreves)
+alreves
+
+# Generar gráfica de sonido
+plot( extractWave(alreves, from = 1, to = 225886) ) 
